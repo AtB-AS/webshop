6b088a700485a7c0dcdaa37a7d29a8e99481b9cc
Martin Jenssen <martin.jenssen@atb.no>
2021-05-04 07:08:36 +0200

    Add Checkbox UI component

---
+++ src/elm/Ui/Input/Checkbox.elm
@@ -0,0 +1,95 @@
+module Ui.Input.Checkbox exposing
+    ( Checkbox
+    , init
+    , setAttributes
+    , setChecked
+    , setId
+    , setName
+    , setOnCheck
+    , setTitle
+    , view
+    )
+
+import Fragment.Icon as Icon
+import Html as H exposing (Attribute, Html)
+import Html.Attributes as A
+import Html.Events as E
+
+
+type alias Checkbox msg =
+    { id : String
+    , name : String
+    , title : String
+    , onCheck : Maybe (Bool -> msg)
+    , checked : Bool
+    , attributes : List (Attribute msg)
+    }
+
+
+init : String -> Checkbox msg
+init id =
+    { id = id
+    , name = ""
+    , title = ""
+    , checked = False
+    , onCheck = Nothing
+    , attributes = []
+    }
+
+
+setId : String -> Checkbox msg -> Checkbox msg
+setId id opts =
+    { opts | id = id }
+
+
+setName : String -> Checkbox msg -> Checkbox msg
+setName name opts =
+    { opts | name = name }
+
+
+setTitle : String -> Checkbox msg -> Checkbox msg
+setTitle title opts =
+    { opts | title = title }
+
+
+setChecked : Bool -> Checkbox msg -> Checkbox msg
+setChecked checked opts =
+    { opts | checked = checked }
+
+
+setOnCheck : Maybe (Bool -> msg) -> Checkbox msg -> Checkbox msg
+setOnCheck onCheck opts =
+    { opts | onCheck = onCheck }
+
+
+setAttributes : List (Attribute msg) -> Checkbox msg -> Checkbox msg
+setAttributes attributes opts =
+    { opts | attributes = attributes }
+
+
+view : Checkbox msg -> Html msg
+view { id, name, title, onCheck, checked, attributes } =
+    H.div []
+        [ H.input
+            ([ A.type_ "checkbox"
+             , A.id id
+             , A.name name
+             , A.class "ui-input-checkbox__input"
+             , A.checked checked
+             ]
+                ++ (onCheck |> Maybe.map (E.onCheck >> List.singleton) |> Maybe.withDefault [])
+                ++ attributes
+            )
+            []
+        , H.label [ A.for id, A.class "ui-input-checkbox" ]
+            [ H.div [ A.class "ui-input-checkbox__box" ]
+                [ case checked of
+                    True ->
+                        Icon.checkOn
+
+                    False ->
+                        Icon.checkOff
+                ]
+            , H.div [ A.class "ui-input-checkbox__title" ] [ H.text title ]
+            ]
+        ]
