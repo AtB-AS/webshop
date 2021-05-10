f8a3e0c3eee85b12c7072191d574c52b46f268b4
Mikael Brevik <mikaelbre@gmail.com>
2021-05-10 20:16:27 +0200

    refactor: removes unused and old code

--- src/elm/Page/Overview.elm
+++ src/elm/Page/Overview.elm
@@ -1,17 +1,15 @@
 module Page.Overview exposing (Model, Msg(..), init, subscriptions, update, view)
 
 import Base exposing (AppInfo)
-import Data.FareContract exposing (FareContract, TravelRight(..))
+import Data.FareContract exposing (FareContract, FareContractState(..), TravelRight(..))
 import Data.RefData exposing (LangString(..))
 import Data.Webshop exposing (Inspection, Token)
-import Dict exposing (Dict)
 import Environment exposing (Environment)
 import Fragment.Button as Button
 import Fragment.Icon as Icon
 import GlobalActions as GA
 import Html as H exposing (Html)
 import Html.Attributes as A
-import Html.Events as E
 import Html.Extra
 import Http
 import Json.Decode as Decode exposing (Decoder)
@@ -27,7 +25,7 @@ import Ui.Heading
 import Ui.Message as Message
 import Ui.Section
 import Ui.TicketDetails
-import Util.Format as Format
+import Util.FareContract
 import Util.Maybe
 import Util.Status exposing (Status(..))
 
@@ -83,8 +81,8 @@ update msg env model =
                     let
                         -- Only store tickets that are valid into the future.
                         tickets =
-                            fareContracts
-                                |> List.filter (\{ validTo } -> isValid validTo model.currentTime)
+                            Debug.log "fareContracts" fareContracts
+                                -- |> List.filter (.state >> (==) FareContractStateActivated)
                                 |> List.sortBy (.created >> .timestamp)
                                 |> List.reverse
 
@@ -244,9 +242,7 @@ viewMain : Shared -> Model -> Html Msg
 viewMain shared model =
     let
         tickets =
-            List.filter
-                (\{ validTo } -> isValid validTo model.currentTime)
-                model.tickets
+            Util.FareContract.filterValidNow model.currentTime model.tickets
 
         infoMessage =
             Ui.Section.init
@@ -261,7 +257,7 @@ viewMain shared model =
                     ]
 
               else
-                H.div [] (infoMessage :: viewPending model :: viewTicketCards shared model)
+                H.div [] (infoMessage :: viewPending model :: viewTicketCards shared tickets model)
             ]
 
 
@@ -282,9 +278,9 @@ viewPending model =
             H.text ""
 
 
-viewTicketCards : Shared -> Model -> List (Html Msg)
-viewTicketCards shared model =
-    model.tickets
+viewTicketCards : Shared -> List FareContract -> Model -> List (Html Msg)
+viewTicketCards shared tickets model =
+    tickets
         |> List.filter (\{ validTo } -> isValid validTo model.currentTime)
         |> List.map
             (\f ->
@@ -297,296 +293,9 @@ viewTicketCards shared model =
             )
 
 
-
--- ++ (model.tickets
---         |> List.filter (\{ validTo } -> isValid validTo model.currentTime)
---         |> List.map (viewTicketCard shared model)
---    )
-
-
-pluralFormat : Int -> String -> String -> String
-pluralFormat value singular plural =
-    String.fromInt value
-        ++ " "
-        ++ (if value == 1 then
-                singular
-
-            else
-                plural
-           )
-
-
-timeFormat : Int -> String -> String -> String
-timeFormat time suffix fallback =
-    if time >= 86400 then
-        pluralFormat (time // 86400) "dag" "dager" ++ " " ++ suffix
-
-    else if time >= 7200 then
-        pluralFormat (time // 3600) "time" "timer" ++ " " ++ suffix
-
-    else if time >= 120 then
-        pluralFormat (time // 60) "minutt" "minutter" ++ " " ++ suffix
-
-    else if time >= 1 then
-        pluralFormat time "sekund" "sekunder" ++ " " ++ suffix
-
-    else
-        fallback
-
-
-timeLeftFormat : Int -> String
-timeLeftFormat time =
-    timeFormat time "igjen" "utløper straks"
-
-
-timeAgoFormat : Int -> String
-timeAgoFormat time =
-    timeFormat time "siden" "nå nettopp"
-
-
-frequency : List comparable -> Dict comparable Int
-frequency =
-    List.foldl
-        (\item ->
-            Dict.update item (Maybe.map ((+) 1) >> Maybe.withDefault 1 >> Just)
-        )
-        Dict.empty
-
-
-viewValidity : Int -> Time.Posix -> Html msg
-viewValidity to posixNow =
-    let
-        now =
-            Time.posixToMillis posixNow
-    in
-        if now > to then
-            H.text <| timeAgoFormat <| (now - to) // 1000
-
-        else
-            H.text <| timeLeftFormat <| (to - now) // 1000
-
-
 isValid : Int -> Time.Posix -> Bool
 isValid to posixNow =
     to >= Time.posixToMillis posixNow
-
-
-viewTicketCard : Shared -> Model -> FareContract -> Html Msg
-viewTicketCard shared model fareContract =
-    let
-        expanded =
-            model.expanded == Just fareContract.orderId
-
-        userProfilesList =
-            fareContract.travelRights
-                |> List.filterMap
-                    (\type_ ->
-                        case type_ of
-                            SingleTicket ticket ->
-                                Just ticket.userProfileRef
-
-                            PeriodTicket ticket ->
-                                Just ticket.userProfileRef
-
-                            UnknownTicket _ ->
-                                Nothing
-                    )
-
-        userProfiles =
-            userProfilesList
-                |> frequency
-                |> Dict.map (viewUserProfile shared)
-                |> Dict.values
-
-        userInfo =
-            case userProfiles of
-                [ userProfile ] ->
-                    userProfile
-
-                [ _, _ ] ->
-                    String.join ", " userProfiles
-
-                _ ->
-                    String.fromInt (List.length userProfilesList) ++ " tickets"
-
-        fareProduct =
-            fareContract.travelRights
-                |> List.filterMap
-                    (\type_ ->
-                        case type_ of
-                            SingleTicket ticket ->
-                                Just ticket.fareProductRef
-
-                            PeriodTicket ticket ->
-                                Just ticket.fareProductRef
-
-                            UnknownTicket _ ->
-                                Nothing
-                    )
-                |> frequency
-                |> Dict.map (viewFareProduct shared)
-                |> Dict.values
-                |> List.filter ((/=) "")
-                |> String.join ", "
-
-        zones =
-            fareContract.travelRights
-                |> List.filterMap
-                    (\type_ ->
-                        case type_ of
-                            SingleTicket ticket ->
-                                Just ticket.tariffZoneRefs
-
-                            PeriodTicket ticket ->
-                                Just ticket.tariffZoneRefs
-
-                            UnknownTicket _ ->
-                                Nothing
-                    )
-                |> List.concat
-                |> frequency
-                |> Dict.map
-                    (\ref _ ->
-                        shared.tariffZones
-                            |> List.filter (.id >> (==) ref)
-                            |> List.head
-                            |> Maybe.map (\zone -> "Sone " ++ langString zone.name)
-                            |> Maybe.withDefault ""
-                    )
-                |> Dict.values
-                |> List.filter ((/=) "")
-    in
-        H.div [ A.class "section-box" ]
-            [ H.div [ A.class "ticket-header" ]
-                [ Icon.wrapper 20 Icon.bus
-                , H.div [ A.class "product-name" ]
-                    [ if fareProduct == "" then
-                        H.text "Ukjent produkt"
-
-                      else
-                        H.text fareProduct
-                    ]
-                , H.div [ A.class "zone-name" ]
-                    [ case zones of
-                        [] ->
-                            H.text "Ingen soner"
-
-                        [ zone ] ->
-                            H.text zone
-
-                        _ ->
-                            H.text <| String.fromInt (List.length zones) ++ " soner"
-                    ]
-                ]
-            , H.div [ A.class "card-content" ]
-                [ H.div
-                    [ A.class "ticket-info" ]
-                    [ viewValidity fareContract.validTo model.currentTime ]
-                ]
-            , if expanded then
-                H.div []
-                    [ H.label [] [ H.text "Kjøpsinformasjon" ]
-                    , H.div [ A.class "metadata-list" ]
-                        [ H.div [] [ H.text <| "Kjøpt " ++ Format.dateTime fareContract.created ]
-                        , H.div [] [ H.text <| "Totalt kr " ++ formatTotal fareContract.totalAmount ]
-                        , H.div [] [ H.text <| "Betalt med " ++ formatPaymentType fareContract.paymentType ]
-                        , H.div [] [ H.text <| "Ordre-ID: " ++ fareContract.orderId ]
-                        ]
-                    ]
-
-              else
-                H.text ""
-            , H.div
-                [ A.style "display" "flex"
-                , A.style "cursor" "pointer"
-                , E.onClick (ToggleTicket fareContract.orderId)
-                ]
-                (H.div
-                    [ A.style "flex-grow" "1"
-                    , A.style "font-weight" "500"
-                    ]
-                    [ H.text "Detaljer" ]
-                    :: (if expanded then
-                            [ H.span [ A.style "margin-right" "12px" ] [ H.text "Skjul" ]
-                            , Icon.wrapper 20 Icon.upArrow
-                            ]
-
-                        else
-                            [ H.span [ A.style "margin-right" "12px" ] [ H.text "Vis" ]
-                            , Icon.wrapper 20 Icon.downArrow
-                            ]
-                       )
-                )
-            ]
-
-
-formatTotal : Maybe String -> String
-formatTotal value =
-    case Maybe.andThen String.toFloat value of
-        Just floatValue ->
-            Format.float floatValue 2
-
-        Nothing ->
-            "??"
-
-
-formatPaymentType : List String -> String
-formatPaymentType types =
-    case types of
-        [] ->
-            "??"
-
-        [ "VIPPS" ] ->
-            "Vipps"
-
-        [ "VISA" ] ->
-            "bankkort"
-
-        _ ->
-            "??"
-
-
-langString : LangString -> String
-langString (LangString _ value) =
-    value
-
-
-multiString : Int -> String -> String
-multiString count str =
-    if String.isEmpty str || count < 1 then
-        ""
-
-    else if count == 1 then
-        str
-
-    else
-        String.fromInt count ++ "× " ++ str
-
-
-viewUserProfile : Shared -> String -> Int -> String
-viewUserProfile shared userProfile count =
-    shared.userProfiles
-        |> List.filter
-            (\entry ->
-                entry.id == userProfile
-            )
-        |> List.map (.name >> langString)
-        |> List.head
-        |> Maybe.withDefault ""
-        |> multiString count
-
-
-viewFareProduct : Shared -> String -> Int -> String
-viewFareProduct shared fareProduct _ =
-    shared.fareProducts
-        |> List.filter
-            (\entry ->
-                entry.id == fareProduct
-            )
-        |> List.map (.name >> langString)
-        |> List.head
-        |> Maybe.withDefault ""
-        |> multiString 1
 
 
 subscriptions : Model -> Sub Msg
