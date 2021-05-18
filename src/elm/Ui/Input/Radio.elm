720763124ee075c8f7537fc995d828d6ed58cafb
Mikael Brevik <mikaelbre@gmail.com>
2021-05-18 12:22:34 +0200

    fix: issue with subtitle for radio button not being block element

--- src/elm/Ui/Input/Radio.elm
+++ src/elm/Ui/Input/Radio.elm
@@ -106,7 +106,11 @@ view { id, name, title, onCheck, checked, subtitle, attributes } =
         , H.label [ A.for id, A.class "ui-input-radio" ]
             [ H.span [ A.class "ui-input-radio__title" ]
                 [ H.span [] [ H.text title ]
-                , Html.Extra.viewMaybe (\t -> Text.textContainer H.span (Just Text.SecondaryColor) <| Text.Tertiary [ H.text t ]) subtitle
+                , Html.Extra.viewMaybe
+                    (\t ->
+                        H.span [ A.class "ui-input-radio__subtitle" ] [ Text.textContainer H.span (Just Text.SecondaryColor) <| Text.Tertiary [ H.text t ] ]
+                    )
+                    subtitle
                 ]
             , H.span [ A.class "ui-input-radio__box" ] []
             ]
