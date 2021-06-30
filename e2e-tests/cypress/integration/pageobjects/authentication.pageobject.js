2cb77a8fa8aca5f4c8ce9da97856c1034be304f9
tormoseng <tor@moseng.no>
2021-06-30 10:22:22 +0200

    prettify

--- e2e-tests/cypress/integration/pageobjects/authentication.pageobject.js
+++ e2e-tests/cypress/integration/pageobjects/authentication.pageobject.js
@@ -1,19 +1,32 @@
 export const auth = {
-  setEmail: email => cy.get("input#email").type(email),
-  setPassword: pw => cy.get("input#password").type(pw),
-  createProfile: () => cy.get("button").contains("Opprett profil").click({ force: true }),
-  useEmailAsLogIn: () => cy.get(".ui-section").find("a").contains("Jeg vil heller bruke e-post").click(),
-  createNewEmailProfile: () => cy.get(".ui-section").find("a").contains("Opprett en ny profil").click(),
-  forgotPassword: () => cy.get("a").contains("Glemt passord").click(),
-  info: () => cy.get(".ui-section"),
-  resetPassword: () => cy.get("button").contains("Tilbakestill passord").click({ force: true })
+    setEmail: (email) => cy.get('input#email').type(email),
+    setPassword: (pw) => cy.get('input#password').type(pw),
+    createProfile: () =>
+        cy.get('button').contains('Opprett profil').click({ force: true }),
+    useEmailAsLogIn: () =>
+        cy
+            .get('.ui-section')
+            .find('a')
+            .contains('Jeg vil heller bruke e-post')
+            .click(),
+    createNewEmailProfile: () =>
+        cy
+            .get('.ui-section')
+            .find('a')
+            .contains('Opprett en ny profil')
+            .click(),
+    forgotPassword: () => cy.get('a').contains('Glemt passord').click(),
+    info: () => cy.get('.ui-section'),
+    resetPassword: () =>
+        cy.get('button').contains('Tilbakestill passord').click({ force: true })
 };
 
 //External Firebase confirmation page
 export const firebase = {
-  confirm: () => cy.get("button").contains("Continue").click({ force: true }),
-  infoText: () => cy.get(".firebaseui-text"),
-  setNewPassword: pw =>  cy.get('input[name="newPassword"').type(pw),
-  savePassword: () => cy.get("button").contains("Save").click({ force: true }),
-  title: () => cy.get(".firebaseui-title")
-}
+    confirm: () => cy.get('button').contains('Continue').click({ force: true }),
+    infoText: () => cy.get('.firebaseui-text'),
+    setNewPassword: (pw) => cy.get('input[name="newPassword"').type(pw),
+    savePassword: () =>
+        cy.get('button').contains('Save').click({ force: true }),
+    title: () => cy.get('.firebaseui-title')
+};
--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -15,7 +15,7 @@
 // Helper functions
 // ***********************************************
 
-// Print summary table after CI run
+// Print summary table after CI run - used in the a11y tests
 function terminalLog(violations) {
     /*
     cy.task(
@@ -33,27 +33,26 @@ function terminalLog(violations) {
             description,
             nodes: nodes.length
         })
-    )
-    cy.task('table', violationData)
+    );
+    cy.task('table', violationData);
 }
 
 // ***********************************************
 // Overwrite commands
 // ***********************************************
 
-
 // ***********************************************
 // Custom commands
 // ***********************************************
 
 // Re-define 'cy.checkA11y()' with pre-set tags from 'cypress.json#env.a11yTags'
-Cypress.Commands.add("a11yCheck", (context, inOptions) => {
+Cypress.Commands.add('a11yCheck', (context, inOptions) => {
     //Add tags
     let options = {
         runOnly: {
-            type: 'tag',
+            type: 'tag'
             //values: ['wcag2a', 'wcag21a']
-        },
+        }
         /*
         // Example of a rule
         // https://www.deque.com/axe/core-documentation/api-documentation/
@@ -61,26 +60,31 @@ Cypress.Commands.add("a11yCheck", (context, inOptions) => {
             'color-contrast': { enabled: false },
         }
          */
+    };
+    let values = [];
+    for (let i = 0; i < Cypress.env('a11yTags').length; i++) {
+        values.push(Cypress.env('a11yTags')[i]);
     }
-    let values = []
-    for (let i = 0; i < Cypress.env("a11yTags").length; i++){
-        values.push(Cypress.env("a11yTags")[i])
-    }
-    options.runOnly.values = values
+    options.runOnly.values = values;
 
     //Add rules
-    if (inOptions !== null && inOptions.rules !== null){
-        options.rules = inOptions.rules
+    if (inOptions !== null && inOptions.rules !== null) {
+        options.rules = inOptions.rules;
     }
 
     return cy.injectAxe().then(() => {
-        cy.checkA11y(context, options, terminalLog, Cypress.env("a11ySkipFailures"))
-    })
-})
+        cy.checkA11y(
+            context,
+            options,
+            terminalLog,
+            Cypress.env('a11ySkipFailures')
+        );
+    });
+});
 
 //Log out if authorized
-Cypress.Commands.add("visitMainAsNotAuthorized", () => {
-        /*
+Cypress.Commands.add('visitMainAsNotAuthorized', () => {
+    /*
         //Try with dark/light modes - not working
         //https://www.cypress.io/blog/2019/12/13/test-your-web-app-in-dark-mode/
         .visit("", {
@@ -93,89 +97,106 @@ Cypress.Commands.add("visitMainAsNotAuthorized", () => {
             },
         })
         */
-    cy
-        .visit("")
-        .wait(2000)
-        //.get(".ui-section").find("button").contains("neste").click({force:true})
-        //.wait(500)
+    cy.visit('').wait(2000);
     cy.window().then((win) => {
-            //window.localStorage.getItem("loggedIn").length
-            if (window.localStorage.getItem("loggedIn") !== null){
-                console.log("*null* " + window.localStorage.getItem("loggedIn"))
-                cy.logOut()
-            }
-            else {
-                cy
-                    .get(".ui-section").find("button").contains("neste").click({force:true})
-                    .wait(500)
-            }
-        })
-})
+        //window.localStorage.getItem("loggedIn").length
+        if (window.localStorage.getItem('loggedIn') !== null) {
+            console.log('*null* ' + window.localStorage.getItem('loggedIn'));
+            cy.logOut();
+        } else {
+            cy.get('.ui-section')
+                .find('button')
+                .contains('neste')
+                .click({ force: true })
+                .wait(500);
+        }
+    });
+});
 
 //Log in if not authorized
-Cypress.Commands.add("visitMainAsAuthorized", (userEmail = Cypress.env("email"), userPassword = Cypress.env("password")) => {
-    cy
-        .visit("")
-        .wait(2000)
-        //.get(".ui-section").find("button").contains("neste").click({force:true})
-        //.wait(500)
-    cy.window().then((win) => {
+Cypress.Commands.add(
+    'visitMainAsAuthorized',
+    (
+        userEmail = Cypress.env('email'),
+        userPassword = Cypress.env('password')
+    ) => {
+        cy.visit('').wait(2000);
+        cy.window().then((win) => {
             //window.localStorage.getItem("loggedIn").length
-            if (window.localStorage.getItem("loggedIn") === null){
-                console.log("*null* " + window.localStorage.getItem("loggedIn"))
-                cy.logIn(userEmail, userPassword)
+            if (window.localStorage.getItem('loggedIn') === null) {
+                console.log(
+                    '*null* ' + window.localStorage.getItem('loggedIn')
+                );
+                cy.logIn(userEmail, userPassword);
             }
-        })
-    cy.get("h1.pageHeader__logo").click()
-    cy.get("h2.ui-pageHeader__title").contains("Mine billetter")
-    cy.get("h2.ui-pageHeader__title").should("contain", "Mine billetter")
-})
+        });
+        cy.get('h1.pageHeader__logo').click();
+        cy.get('h2.ui-pageHeader__title').contains('Mine billetter');
+        cy.get('h2.ui-pageHeader__title').should('contain', 'Mine billetter');
+    }
+);
 
 //Log in
-Cypress.Commands.add("logIn", (userEmail, userPassword) => {
-    cy.intercept("POST", "**/identitytoolkit/v3/relyingparty/verifyPassword**").as("login")
-    cy.intercept("POST", "**/identitytoolkit/v3/relyingparty/getAccountInfo**").as("accountInfo")
-    cy.intercept("POST", "**/v1/token**").as("refreshToken")
+Cypress.Commands.add('logIn', (userEmail, userPassword) => {
+    cy.intercept(
+        'POST',
+        '**/identitytoolkit/v3/relyingparty/verifyPassword**'
+    ).as('login');
+    cy.intercept(
+        'POST',
+        '**/identitytoolkit/v3/relyingparty/getAccountInfo**'
+    ).as('accountInfo');
+    cy.intercept('POST', '**/v1/token**').as('refreshToken');
 
-    cy
-        .get(".ui-section").find("button").contains("neste").click({force:true})
-        .wait(500)
-    cy.get(".ui-section").find("a").contains("Jeg vil heller bruke e-post").click()
-    cy.get("#email").type(userEmail)
-    cy.get("#password").type(userPassword)
-    cy.wait(100)
-    cy.get("button[type=submit]").contains("Logg inn").click()
-    cy.wait(["@login", "@accountInfo","@refreshToken"])
-    cy.wait(200)
-})
+    cy.get('.ui-section')
+        .find('button')
+        .contains('neste')
+        .click({ force: true })
+        .wait(500);
+    cy.get('.ui-section')
+        .find('a')
+        .contains('Jeg vil heller bruke e-post')
+        .click();
+    cy.get('#email').type(userEmail);
+    cy.get('#password').type(userPassword);
+    cy.wait(100);
+    cy.get('button[type=submit]').contains('Logg inn').click();
+    cy.wait(['@login', '@accountInfo', '@refreshToken']);
+    cy.wait(200);
+});
 
 //Log out
-Cypress.Commands.add("logOut", () => {
-    cy.get("button.pageHeader__nav__logout").click({force: true})
-    cy.reload()
-    cy
-        .get(".ui-section").find("button").contains("neste").click({force:true})
-        .wait(500)
-    cy.get("h2.ui-section__headerTitle").should("contain", "Velkommen til AtBs nettbutikk")
-})
+Cypress.Commands.add('logOut', () => {
+    cy.get('button.pageHeader__nav__logout').click({ force: true });
+    cy.reload();
+    cy.get('.ui-section')
+        .find('button')
+        .contains('neste')
+        .click({ force: true })
+        .wait(500);
+    cy.get('h2.ui-section__headerTitle').should(
+        'contain',
+        'Velkommen til AtBs nettbutikk'
+    );
+});
 
 //Buy a ticket
-Cypress.Commands.add("buyTicket", () => {
-    const script = "python3 python-scripts/buyTicket.py --email " + Cypress.env("email") + " --password " + Cypress.env("password")
-    cy
-        //.exec('python3 python-scripts/buyTicket.py --email --password')
-        .exec(script, { timeout: 60000 })
-        .its('stdout').then($stdout => {
-            if($stdout.includes('ORDER_ID')){
-                let refId = $stdout.replace(/^ORDER_ID:\s(.*)/g, "$1")
-                console.log("Ticket refId " + refId)
-                return refId
+Cypress.Commands.add('buyTicket', () => {
+    const script =
+        'python3 python-scripts/buyTicket.py --email ' +
+        Cypress.env('email') +
+        ' --password ' +
+        Cypress.env('password');
+    cy.exec(script, { timeout: 60000 })
+        .its('stdout')
+        .then(($stdout) => {
+            if ($stdout.includes('ORDER_ID')) {
+                let refId = $stdout.replace(/^ORDER_ID:\s(.*)/g, '$1');
+                console.log('Ticket refId ' + refId);
+                return refId;
+            } else {
+                console.log('Ticket ERROR');
+                return 'ERROR';
             }
-            else{
-                console.log('Ticket ERROR')
-                return 'ERROR'
-            }
-    })
-})
-
-
+        });
+});
