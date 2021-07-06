977bd4b4f4ea9feadcdd3aa0edb9dc7b1742e786
Mikael Brevik <mikaelbre@gmail.com>
2021-07-06 15:01:35 +0200

    fix: dont show carnets that have no more accesses and no active access

--- src/elm/Util/FareContract.elm
+++ src/elm/Util/FareContract.elm
@@ -1,6 +1,6 @@
 module Util.FareContract exposing (filterValidNow, isValid)
 
-import Data.FareContract exposing (FareContract, FareContractState(..))
+import Data.FareContract exposing (FareContract, FareContractState(..), TravelRight(..))
 import Time
 
 
@@ -8,6 +8,31 @@ filterValidNow : Time.Posix -> List FareContract -> List FareContract
 filterValidNow now fareContracts =
     fareContracts
         |> List.filter (.validTo >> isValid now)
+        |> List.filter (hasValidTravelRight now)
+
+
+{-| Check if a FareContract has valid travelRights.
+Currently explicitly checking for the carnet type and if
+-}
+hasValidTravelRight : Time.Posix -> FareContract -> Bool
+hasValidTravelRight now contract =
+    contract.travelRights
+        |> List.any
+            (\travelRight ->
+                case travelRight of
+                    CarnetTicket carnetType ->
+                        let
+                            hasTicketsLeft =
+                                carnetType.numberOfUsedAccesses < carnetType.maximumNumberOfAccesses
+
+                            hasActiveAccess =
+                                List.any (.endDateTime >> .timestamp >> isValid now) carnetType.usedAccesses
+                        in
+                            hasTicketsLeft || hasActiveAccess
+
+                    _ ->
+                        True
+            )
 
 
 isValid : Time.Posix -> Int -> Bool
