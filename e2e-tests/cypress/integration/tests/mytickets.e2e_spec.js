52767335248352cd3b7073e7880f9f685983c3ea
tormoseng <tor@moseng.no>
2021-08-20 14:05:11 +0200

    e2e for myprofile

--- e2e-tests/cypress/integration/tests/mytickets.e2e_spec.js
+++ e2e-tests/cypress/integration/tests/mytickets.e2e_spec.js
@@ -71,10 +71,67 @@ describe('ticket details', () => {
         cy.visitMainAsAuthorized();
     });
 
+    it('carnet ticket should be correct', () => {
+        const order_id = 'R72EMYQA';
+        const validFrom = '20.08.2021 - ' + getValidCarnetHours(11) + ':50'
+        const validTo = '21.08.2022 - ' + getValidCarnetHours(11) + ':50'
+        const header1 = '10 klipp igjen'
+        const header2 = 'Ingen aktive klipp'
+        const type = 'Klippekort (10 billetter)';
+        const zones = 'Reise i 1 sone (Sone A)';
+        const traveller = '1 Voksen';
+        const payment = 'Vipps';
+
+        verify.verifyHeader('h2', 'Mine billetter');
+
+        //Verify overview
+        mytickets.ticket(order_id).then(($ticket) => {
+            mytickets.carnetTicketIconIsCorrect($ticket);
+            mytickets.carnetTicketHeader($ticket)
+                .should('contain', header1)
+                .and('contain', header2)
+            mytickets
+                .ticketSummary($ticket)
+                .should('contain', type)
+                .and('contain', zones)
+                .and('contain', traveller);
+            mytickets.ticketIsCollapsed($ticket, true);
+            mytickets.ticketDetails($ticket).should('not.be.visible');
+        });
+
+        //Verify details
+        mytickets.ticket(order_id).then(($ticket) => {
+            mytickets.showDetails($ticket);
+            mytickets.ticketIsCollapsed($ticket, false);
+            mytickets
+                .ticketDetails($ticket)
+                .should('be.visible')
+                .and('contain', 'Gyldig fra')
+                .and('contain', validFrom)
+                .and('contain', 'Gyldig til')
+                .and('contain', validTo)
+                .and('contain', 'Kjøpstidspunkt')
+                .and('contain', 'Betalt med')
+                .and('contain', payment)
+                .and('contain', 'Ordre-ID')
+                .and('contain', order_id);
+            mytickets.ticketReceipt($ticket)
+                .should("be.enabled")
+        });
+
+        //Hide details
+        mytickets.ticket(order_id).then(($ticket) => {
+            mytickets.hideDetails($ticket);
+            mytickets.ticketIsCollapsed($ticket, true);
+            mytickets.ticketDetails($ticket).should('not.be.visible');
+        });
+    });
+
     //** NOTE! Only valid until pre-set date **
-    it('future ticket should be waiting and correct', () => {
-        const order_id = 'HUCVIBHX';
-        const validFrom = getValidFrom()
+    it('future period ticket should be waiting and correct', () => {
+        const order_id = Cypress.env("futureTicketOrderId");
+        const validFrom = getValidTime(12, 0)
+        const validTo = getValidTime(12, 0, 7)
         const header = 'Gyldig fra ' + validFrom;
         const type = '7-dagersbillett';
         const zones = 'Reise i 3 soner (Sone A til C1)';
@@ -106,6 +163,7 @@ describe('ticket details', () => {
                 .and('contain', 'Gyldig fra')
                 .and('contain', validFrom)
                 .and('contain', 'Gyldig til')
+                .and('contain', validTo)
                 .and('contain', 'Kjøpstidspunkt')
                 .and('contain', 'Betalt med')
                 .and('contain', payment)
@@ -201,13 +259,45 @@ describe('ticket details', () => {
         });
     }
 
-    //Different timezone on the host running GH Actions
-    function getValidFrom(){
+    function getValidTime(hours, minutes, increaseDays = 0, ){
+        let myDate = new Date();
+        let days = Number.parseInt(Cypress.env('futureTicketStartDay')) + increaseDays
+        myDate.setFullYear(Cypress.env('futureTicketStartYear'))
+        myDate.setMonth(Cypress.env('futureTicketStartMonth') - 1)
+        myDate.setDate(days)
+        //Different timezone on the host running GH Actions
         if (Cypress.env('runOnGitHub')){
-            return Cypress.env('futureTicketStartDay') + "." + Cypress.env('futureTicketStartMonth') + "." + Cypress.env('futureTicketStartYear') + " - 10:00"
+            myDate.setHours(hours - 2)
         }
         else {
-            return Cypress.env('futureTicketStartDay') + "." + Cypress.env('futureTicketStartMonth') + "." + Cypress.env('futureTicketStartYear') + " - 12:00"
+            myDate.setHours(hours)
+        }
+        myDate.setMinutes(minutes)
+
+        let dd = myDate.getDate();
+        let mm = myDate.getMonth() + 1;
+        let yyyy = myDate.getFullYear();
+        let HH = myDate.getHours()
+        let MM = myDate.getMinutes()
+
+        //Padding
+        if(dd < 10){ dd = '0' + dd }
+        if(mm < 10){ mm = '0' + mm }
+        if(HH < 10){ HH = '0' + HH }
+        if(MM < 10){ MM = '0' + MM }
+
+        return dd + '.' + mm + '.' + yyyy + " - " + HH + ":" + MM
+    }
+
+    //Different timezone on the host running GH Actions
+    function getValidCarnetHours(hours){
+        if (Cypress.env('runOnGitHub')){
+            let hh = hours - 2
+            if (hh < 10){ hh = '0' + hh}
+            return hh
+        }
+        else {
+            return hours
         }
     }
 });
