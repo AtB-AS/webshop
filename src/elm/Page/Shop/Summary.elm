3bade41df04e49cd9cd452438adf7a9c0a918e14
Gøran Dalum <goran.dalum@kantega.no>
2021-08-31 14:11:50 +0200

    fix: Show correct card expiration date

--- src/elm/Page/Shop/Summary.elm
+++ src/elm/Page/Shop/Summary.elm
@@ -430,7 +430,7 @@ recurringPaymentRadio model recurringPayment =
 
         expireString =
             recurringPayment.expiresAt
-                |> TimeUtil.isoStringToMonthAndYear model.query.timeZone
+                |> TimeUtil.isoStringToCardExpirationMonth model.query.timeZone
                 |> Maybe.map (String.append "Utløpsdato ")
     in
         Radio.init (String.fromInt id)
