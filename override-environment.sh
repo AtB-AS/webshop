d8a388db0f4d2aecd730c0479591fbbf61b01cbd
Tore Stensaker Tefre <tore@tefre.it>
2021-10-13 13:42:04 +0200

    copy correct assets directories on build

--- override-environment.sh
+++ override-environment.sh
@@ -20,7 +20,12 @@ else
     APP_ORG=$2
     ASSETS_FOLDER=orgs/assets/$APP_ORG
     ORG_FOLDER=env/$APP_ORG
+    DESTINATION_FOLDER=src/static/
 
+    echo "Copying $APP_ENVIRONMENT favicon.ico for $APP_ORG to static folder"
+    cp $ASSETS_FOLDER/favicon.ico $DESTINATION_FOLDER
+    echo "Copying $APP_ENVIRONMENT icon.svg for $APP_ORG to static folder"
+    cp $ASSETS_FOLDER/icon.svg $DESTINATION_FOLDER
     echo "Copying $APP_ENVIRONMENT assets for $APP_ORG to static folder"
-    cp -r $ASSETS_FOLDER/. src/static/
+    cp -r $ASSETS_FOLDER/org $DESTINATION_FOLDER/
 fi
