b8985c9fcb0ccb51f17e5097c809dd4e61744425
Gøran Dalum <goran.dalum@mittatb.no>
2021-10-20 10:39:33 +0200

    fix: Fetch vat percent from Remote Config (#398)
    The fetching of vat percent was not working, the default value of 6 was
    used by the webshop even though the value in Remote Config was 12.
    
    Fixed this by fetching vat percent in a similar matter as the reference
    data from Remote Config.

--- src/elm/Service/RemoteConfig.elm
+++ src/elm/Service/RemoteConfig.elm
@@ -1,37 +1,13 @@
-port module Service.RemoteConfig exposing (init, onRemoteConfig)
-
-import Data.RefData exposing (LangString(..), UserType(..))
-import Data.RemoteConfig exposing (RemoteConfig)
-import Json.Decode as Decode exposing (Decoder)
-import Json.Decode.Pipeline as DecodeP
+port module Service.RemoteConfig exposing (onVatPercent)
 
 
-init : RemoteConfig
-init =
-    { vat_percent = 6
-    }
-
-
-onRemoteConfig : (Result () RemoteConfig -> msg) -> Sub msg
-onRemoteConfig =
-    remoteConfigDecoder remoteConfigDataDecoder >> remoteConfigData
+onVatPercent : (Float -> msg) -> Sub msg
+onVatPercent =
+    remoteConfigVatPercent
 
 
 
 -- INTERNAL
 
 
-port remoteConfigData : (Decode.Value -> msg) -> Sub msg
-
-
-remoteConfigDataDecoder : Decoder RemoteConfig
-remoteConfigDataDecoder =
-    Decode.succeed RemoteConfig
-        |> DecodeP.required "vat_percent" Decode.int
-
-
-remoteConfigDecoder : Decoder a -> (Result () a -> msg) -> (Decode.Value -> msg)
-remoteConfigDecoder decoder msg =
-    Decode.decodeValue decoder
-        >> Result.mapError (\_ -> ())
-        >> msg
+port remoteConfigVatPercent : (Float -> msg) -> Sub msg
