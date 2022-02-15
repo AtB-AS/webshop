40ca791c83878056c76f64f164e580b7de206a38
tormoseng <tor@moseng.no>
2022-02-15 11:54:17 +0100

    Fix path and update future ticket

--- e2e-tests/cypress/integration/tests/myprofile.e2e_spec.js
+++ e2e-tests/cypress/integration/tests/myprofile.e2e_spec.js
@@ -109,11 +109,11 @@ describe('my profile', () => {
 
     it('should list stored payment cards', () => {
         myprofile.storedPayment("Visa").should("contain", "Visa, **** 0004")
-        myprofile.storedPaymentIcon("Visa").should("have.attr", "src", "/assets/icons/paymentcard-visa.svg")
+        myprofile.storedPaymentIcon("Visa").should("have.attr", "src", "/assets/mono/ticketing/Visa.svg")
         myprofile.storedPaymentExpiry("Visa").should("contain", "Utløpsdato 08/24")
 
         myprofile.storedPayment("MasterCard").should("contain", "MasterCard, **** 0000")
-        myprofile.storedPaymentIcon("MasterCard").should("have.attr", "src", "/assets/icons/paymentcard-mastercard.svg")
+        myprofile.storedPaymentIcon("MasterCard").should("have.attr", "src", "/assets/mono/ticketing/MasterCard.svg")
         myprofile.storedPaymentExpiry("MasterCard").should("contain", "Utløpsdato 06/24")
     })
 
